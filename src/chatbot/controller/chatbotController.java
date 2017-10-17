0bb500c44d7156770566e47b2f6138f5813cd0b5
Amber Bullough <amber@bertbullough.com>
2017-10-17 09:41:23 -0600

    Added gitignore

---
+++ src/chatbot/controller/chatbotController.java
@@ -0,0 +1,9 @@
+package chatbot.controller;
+
+public class chatbotController 
+{
+ public void start()
+ {
+	 
+ }
+}
---
+++ src/chatbot/controller/chatbotRunner.java
@@ -0,0 +1,10 @@
+package chatbot.controller;
+
+public class chatbotRunner 
+{
+ public static void main(String[]args)
+ {
+	 chatbotController chatbot= new chatbotController();
+	 chatbot.start();
+ }
+}
