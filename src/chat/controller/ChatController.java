acf128080925f0483609bab4f9164b98b2f4ca20
Amber Bullough <amber@bertbullough.com>
2017-11-01 08:48:22 -0600

    Added to controller

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,18 +1,53 @@
 package chat.controller;
 
+import chat.model.Chatbot;
 import chat.view.PopupDisplay;
 
 public class ChatController 
 { 
+	private Chatbot chatbot;
+	private Chatframe appFrame;
 	private PopupDisplay display;
 	
 	public ChatController() 
 	{
-	
+		chatbot = new Chatbot("Silly Chatbot");
+		display = new PopupDisplay();
 	}
 
 	public void start()
 	{
+	 String response = display.collectResponse("What do you want to talk about?");
 	 
+	 while (chatbot.lengthChecker(response)&& !chatbot.quitChecker(response))
+	 {
+		 response = popupChat(response);
+	 }
 	}
+	
+	public String intertWithChatbot(String input)
+	{
+		
+	}
+	
+	private String popupChat(String chat)
+	{
+		return null;
+	}
+	
+	public Chatbot getChatbot()
+	{
+		
+	}
+	
+	public PopupDisplay getDisplay()
+	{
+		
+	}
+	
+	public ChatFrame getChatFrame()
+	{
+	
+	}
+	
 }
--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -38,6 +38,7 @@ public class Chatbot
 		buildCuteAnimals();
 		//buildTopics();
 		//buildFollowups();
+		buildQuestions();
 		buildChatbotResponse();
 	}
 	
